9c7a6e5bceb2d2a6ad35721c39600b919b293e66
Morgangoodluckcode <mgoodluck97@gmail.com>
2022-08-20 19:47:03 +0100

    initial commit

---
+++ MyFirstWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApi.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private string[] products =["laptop", "shoe", "bag,"};
+
+
+     [HttpGet]
+     [Route("get-all")]
+     
+    public IActionResult GetAll()
+    {
+
+        return Ok(products);
+    }
+    }
+}
